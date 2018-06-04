fe05a824ca234716f2925fbcf17c6d8ec91cfced
Eliran Maimon and Renee Sarley <renee@>
2018-06-04 16:36:27 -0700

    Clean variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,17 @@ $(function(){
   var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   var suits = ["C", "D", "S", "H"];
   suits.forEach(function(suit){
-    var suitName = suit;
     ranks.forEach(function(rank){
-      $("ul").append("<li id='" + rank + suitName + "'><img src='img/" + rank + suitName + ".png'></li>");
-      $("ul").append("<li id='" + rank + suitName + "b'><img src='img/backOfCard.png'></li>");
-      $("li#" + rank + suitName + "b").hide();
-      $("li#" + rank + suitName).click(function(){
-        $("li#" + rank + suitName).hide();
-        $("li#" + rank + suitName + "b").show();
+      $("ul").append("<li id='" + rank + suit + "'><img src='img/" + rank + suit + ".png'></li>");
+      $("ul").append("<li id='" + rank + suit + "b'><img src='img/backOfCard.png'></li>");
+      $("li#" + rank + suit + "b").hide();
+      $("li#" + rank + suit).click(function(){
+        $("li#" + rank + suit).hide();
+        $("li#" + rank + suit + "b").show();
       });
-      $("li#" + rank + suitName + "b").click(function(){
-        $("li#" + rank + suitName + "b").hide();
-        $("li#" + rank + suitName).show();
+      $("li#" + rank + suit + "b").click(function(){
+        $("li#" + rank + suit + "b").hide();
+        $("li#" + rank + suit).show();
       });
     });
   });
