26f96e4ca6f232314cb3b18ca113f59795ef6e23
Eliran Maimon and Renee Sarley <eliran@>
2018-06-04 14:23:38 -0700

    change project, add click events to cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(function(){
   var cards = [];
-  var ranks = ["one", "two"];
-  var suits = ["clubs", "diamonds"];
+  var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suits = ["C", "D", "S", "H"];
   suits.forEach(function(suit){
     var suitName = suit;
-    alert(suit);
     ranks.forEach(function(rank){
-      $("ul").append("<li>" + rank + " of " + suitName + "</li>");
+      $("ul").append("<li id='" + rank + suitName + "'><img src='img/" + rank + suitName + ".png'></li>");
+      $("li#" + rank + suitName + " img").click(function(){
+        alert("assign original click");
+        $("li#" + rank + suitName).html("<img src='img/backOfCard.png' id='" + rank + suitName + "b'>");
+        $("img#" + rank + suitName + "b").click(function(){
+          alert("assign return click");
+          $("li#" + rank + suitName).html("<img src='img/" + rank + suitName + ".png' id='" + rank + suitName + "'>");
+        });
+      });
+
     });
   });
 });
