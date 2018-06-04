620b566565d3f799904541c5c053cf005d347c1a
Eliran Maimon and Renee Sarley <eliran@>
2018-06-04 15:53:10 -0700

    add click events to show and hide cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(function(){
-  var cards = [];
   var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   var suits = ["C", "D", "S", "H"];
   suits.forEach(function(suit){
     var suitName = suit;
     ranks.forEach(function(rank){
       $("ul").append("<li id='" + rank + suitName + "'><img src='img/" + rank + suitName + ".png'></li>");
-      $("li#" + rank + suitName + " img").click(function(){
-        alert("assign original click");
-        $("li#" + rank + suitName).html("<img src='img/backOfCard.png' id='" + rank + suitName + "b'>");
-        $("img#" + rank + suitName + "b").click(function(){
-          alert("assign return click");
-          $("li#" + rank + suitName).html("<img src='img/" + rank + suitName + ".png' id='" + rank + suitName + "'>");
-        });
+      $("ul").append("<li id='" + rank + suitName + "b'><img src='img/backOfCard.png'></li>");
+      $("li#" + rank + suitName + "b").hide();
+      $("li#" + rank + suitName).click(function(){
+        $("li#" + rank + suitName).hide();
+        $("li#" + rank + suitName + "b").show();
       });
-
+      $("li#" + rank + suitName + "b").click(function(){
+        $("li#" + rank + suitName + "b").hide();
+        $("li#" + rank + suitName).show();
+      });
     });
   });
 });
